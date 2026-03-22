b7262aeb1be92039227ae7af3c7de8e8ebc9be5b
Hanhdrew <andrew.rebbe@gmail.com>
2026-03-21 20:57:03 -0700

    few notes

---
+++ javascript/factory-functions-and-object-oriented-programming.js
@@ -0,0 +1,71 @@
+//object oriented programming:
+
+//recap: objects being a data type - can store other data types inside - key value pairs
+
+//notionally making something like yelp / ubereats
+//use case - if you have thousands of restaurants you need a more efficient way of doing it
+//solution: OOP
+
+const restaurant1 = {
+  name: "The Spicy Spoon",
+  cuisine: "Thai",
+  rating: 4.5,
+};
+
+const restaurant2 = {
+  name: "The Pizza Place",
+  cuisine: "Italian",
+  rating: 4.0,
+  getRating: function () {
+    return `${this.name} has a rating of`;
+  },
+};
+
+//one solution is to function it ex: a function that returns an object
+//this is a factory function
+//this solves one thing but now you have other problems
+//for example you still need to invoke the function many times
+//it also repeatedly stores the same code on each object - which means youre still creating thousands and thousands of objects
+
+function createRestaurant(name, cuisine, rating) {
+  return {
+    name,
+    cuisine,
+    rating,
+    getRating: function () {
+      return `${this.name} has a rating of`;
+    },
+  };
+}
+
+const restaurant3 = createRestaurant("Spice man", "Asian", 4.5);
+
+console.log(restaurant3);
+
+//the solution is called a constructor or constructor function + prototype chain
+//itll take the benefits of a factory function but also solve repeated information
+//allows objects to be dynamic without bloating
+
+//constructors , New, This:
+
+//constructor: what does it look like?
+// - function starts with a capital letter
+// - name of the function is a name of the object
+// - the function does not return anything in the body of the code
+// - you will see the .this keyword in the body of the function
+// - .this keyword represents the object being created
+
+function Restaurant(name, cuisine, rating) {
+  this.name = name;
+  this.cuisine = cuisine;
+  this.rating = rating;
+}
+
+// we use "new" keyword with constructor functions
+
+const rest1 = new Restaurant("Devslopes Diner", "American", 4.5);
+
+//new automates creation and return - they are always used together
+//js considers Restaurant function to be special
+
+//The prototype Chain - the connection between constructor function and objects it creates
---
+++ life-stuff/roadmap.md
@@ -0,0 +1,19 @@
+👉 Frontend → Backend → Systems → Deployment
+
+Core Engineering Stack
+
+JavaScript
+TypeScript
+React
+Node.js / Express
+SQL
+
+Backend / Systems
+
+Go
+
+DevOps / Cloud
+
+Docker
+CI/CD
+AWS
