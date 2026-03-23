eae4864ad2756df810029e818793083101ca8aee
Hanhdrew <andrew.rebbe@gmail.com>
2026-03-22 20:08:31 -0700

    notes for class syntax

---
+++ javascript/class-syntax.js
@@ -0,0 +1,69 @@
+//quick recap
+
+function Restaurant(name, cuisine, rating) {
+  //constructor function
+  this.name = name; //this refers to objects it creates
+  this.cuisine = cuisine; //semi colons are used because its a statement
+  this.rating = rating; //objects are created from "new" keyword
+}
+
+Restaurant.prototype.getRating = function () {
+  //added method to prototype chain
+  return `${this.name} has a rating of ${this.rating}.`;
+};
+
+//problems with this include if you want multiple methods can become confusing
+//the solution is classes
+
+//class syntax is used to organize and maintain constructors and methods more formally
+
+class Circle {
+  // - every time an instance of Circle is created the constructor will be called
+  // - anything IN the constructor will be stored on the instance of the object (stored directly on the object)
+  // - declare a method after your constructor
+  // - declared the same way you declare on an object literal
+  constructor(radius, color) {
+    this.radius = radius;
+    this.color = color;
+  }
+  calcArea() {
+    return Math.PI * this.radius ** 2;
+  }
+  calcCircumference() {
+    return 2 * this.radius * Math.PI;
+  }
+  // - using this keyword is because the method is shared and stored on the object prototype not the object instance
+  static isValidRadius(radius) {
+    return radius > 0 && typeof radius === "number";
+  }
+  //static methods
+  //has to be accessed as a static method
+  //Circle.isValidRadius()
+  //you cannot invoke or call it like a instance method
+}
+
+//create a new circle object
+const myCircle = new Circle(10, "red");
+
+//now we can call it with myCircle.calArea() invoked by parenthesis
+console.log(myCircle.calcArea());
+
+//a few key notes:
+//the reason class methods still use this keyword is because javascript stores the methods on the object prototype NOT the object instance. myCircle = {radius: 10, color: "red" there is no calcArea}
+//if you call myCircle.calcArea() JS will look for calcArea() on myCircle object which doesnt exist
+//meaning it will go up the prototype chain and call this bound to myCircle
+
+//creating instance and static methods
+
+//instance methods - called on an instance exists on the object
+// - ex: calcArea() is called on myCircle which is an instance of the circle class
+// - ex: array methods nameOfAnArray.map()
+// - array methods like .map() filter() reduce() are instance methods because theyre inherited from the Array.prototype() they are not a part of every array
+// - instance methods are specific
+// - calcArea() for example needs this.radius and this.color which are specific values of that circle instance
+
+//static methods - exist on the class doesnt need object
+// - these methods are for logic that does not depend on an instance ie: radius > 0
+// - it does not need .this in order to function or an instance
+// - communicate intent
+// - used for factories, utilities and class level operations
